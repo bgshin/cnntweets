e8d9285a73512f3a2b77187009002f372a593ae6
Bonggun Shin <nomolos79@gmail.com>
2016-09-07 16:23:27 -0400

    .

--- cnntweets/cnn_models/w2v_lex_cnn.py
+++ cnntweets/cnn_models/w2v_lex_cnn.py
@@ -113,6 +113,7 @@ class W2V_LEX_CNN(object):
             b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
             l2_loss += tf.nn.l2_loss(W)/30
             l2_loss += tf.nn.l2_loss(b)/30
+            self._b = b
             self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
             self.predictions = tf.argmax(self.scores, 1, name="predictions")
 
